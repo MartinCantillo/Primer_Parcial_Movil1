10693d5ab5dbc29181b0e8f3838cce42661a48b6
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-09-01 11:55:27 -0500

    pp

--- primer_parcial_movil1/bin/primer_parcial_movil1.dart
+++ primer_parcial_movil1/bin/primer_parcial_movil1.dart
@@ -1,65 +1,39 @@
-import 'dart:io';
-
 class Producto {
-  int codigoP;
-  String descripcionP;
+  String nombre;
+  DateTime fechaVencimiento;
   double precio;
-  int diasVencimiento;
 
-  Producto(this.codigoP, this.descripcionP, this.precio, this.diasVencimiento);
+  Producto(this.nombre, this.precio, this.fechaVencimiento);
 }
 
 class Venta {
-  int codigoV;
-  String descripcionV;
   List<Producto> productos = [];
-
-  Venta(this.codigoV, this.descripcionV);
 
   void agregarProducto(Producto producto) {
     productos.add(producto);
   }
 
   double calcularTotal() {
-    double total = 0;
-    for (Producto producto in productos) {
-      if (producto.diasVencimiento <= 15) {
-        total += (producto.precio * 0.85);
+    double total = 0.0;
+
+    for (var producto in productos) {
+      if (producto.fechaVencimiento.isBefore(DateTime.now().add(Duration(days: 15)))) {
+        total += producto.precio * 0.15;
       } else {
         total += producto.precio;
       }
     }
+
     return total;
   }
 }
 
-void main(List<String> arguments) {
-  Venta venta1 = Venta(1, "Ventas realizadas por Martin :V");
-  print("Ingrese los días de vencimiento para cada producto:");
+void main() {
+  var venta = Venta();
 
-  Producto portatilLenovo = Producto(1, "Lenovo", 100.0, DiasVencimiento());
-  Producto portatilHP = Producto(2, "HP", 90.0, DiasVencimiento());
-  Producto raton = Producto(3, "ratón", 20.0, DiasVencimiento());
-  Producto pcEscritorio =
-      Producto(4, "PC escritorio", 600.0, DiasVencimiento());
+  venta.agregarProducto(Producto('Producto 1', 100.0, DateTime(2023, 10, 1)));
+  venta.agregarProducto(Producto('Producto 2', 50.0, DateTime(2023, 9, 16)));
 
-  venta1.agregarProducto(portatilLenovo);
-  venta1.agregarProducto(portatilHP);
-  venta1.agregarProducto(raton);
-  venta1.agregarProducto(pcEscritorio);
-
-  double total = venta1.calcularTotal();
-
-  print("Ventas realizadas por Martin:");
-  for (var producto in venta1.productos) {
-    print("Producto: ${producto.descripcionP}");
-    print("Precio: ${producto.precio}");
-  }
-  print("Total de la venta = $total");
-}
-
-int DiasVencimiento() {
-  stdout.write("Días de vencimiento: ");
-  String input = stdin.readLineSync()!;
-  return int.parse(input);
+  var total = venta.calcularTotal();
+  print('Total a pagar: \$${total.toStringAsFixed(2)}');
 }
