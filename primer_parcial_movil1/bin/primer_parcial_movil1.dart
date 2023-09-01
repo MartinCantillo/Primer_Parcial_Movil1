7a5a5174ea8ed0bc5a90a05bec9221f1d1d6c33e
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-09-01 12:12:06 -0500

    Mejorando

--- primer_parcial_movil1/bin/primer_parcial_movil1.dart
+++ primer_parcial_movil1/bin/primer_parcial_movil1.dart
@@ -1,10 +1,4 @@
-class Producto {
-  String nombre;
-  DateTime fechaVencimiento;
-  double precio;
 
-  Producto(this.nombre, this.precio, this.fechaVencimiento);
-}
 
 class Venta {
   List<Producto> productos = [];
@@ -31,9 +25,21 @@ class Venta {
 void main() {
   var venta = Venta();
 
-  venta.agregarProducto(Producto('Producto 1', 100.0, DateTime(2023, 10, 1)));
-  venta.agregarProducto(Producto('Producto 2', 50.0, DateTime(2023, 9, 16)));
-
+  venta.agregarProducto(Producto('Lenovo ', 100.0, DateTime(2023, 10, 1)));
+  venta.agregarProducto(Producto('HP ', 50.0, DateTime(2023, 9, 16)));
+  venta.agregarProducto(Producto('Mouse ', 100.0, DateTime(2023, 10, 1)));
+  venta.agregarProducto(Producto('Escritorio ', 120.0, DateTime(219, 9, 16)));
+  venta.agregarProducto(Producto('ryzen ', 50.0, DateTime(2023, 9, 16)));
+  venta.agregarProducto(Producto('Mouse2 ', 1020.0, DateTime(2023, 10, 1)));
+  venta.agregarProducto(Producto('mouse3 ', 1220.0, DateTime(223, 9, 16)));
   var total = venta.calcularTotal();
   print('Total a pagar: \$${total.toStringAsFixed(2)}');
+}
+
+class Producto {
+  String nombre;
+  DateTime fechaVencimiento;
+  double precio;
+
+  Producto(this.nombre, this.precio, this.fechaVencimiento);
 }
