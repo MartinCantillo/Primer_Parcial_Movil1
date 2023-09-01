9a6e585c22940301b71c717699d8f189341aaf5a
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-09-01 10:47:51 -0500

    first commit

---
+++ primer_parcial_movil1/bin/primer_parcial_movil1.dart
@@ -0,0 +1,5 @@
+import 'package:primer_parcial_movil1/primer_parcial_movil1.dart' as primer_parcial_movil1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${primer_parcial_movil1.calculate()}!');
+}
---
+++ primer_parcial_movil1/test/primer_parcial_movil1_test.dart
@@ -0,0 +1,8 @@
+import 'package:primer_parcial_movil1/primer_parcial_movil1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
