c169e13bbe3fcfdafd0340490df5565f43871365
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-09-01 10:58:51 -0500

    Adding productos and ventas class

--- primer_parcial_movil1/bin/primer_parcial_movil1.dart
+++ primer_parcial_movil1/bin/primer_parcial_movil1.dart
@@ -1,5 +1,27 @@
 import 'package:primer_parcial_movil1/primer_parcial_movil1.dart' as primer_parcial_movil1;
 
+
 void main(List<String> arguments) {
   print('Hello world: ${primer_parcial_movil1.calculate()}!');
 }
+
+class Producto {
+  int codigoP;
+  String nombre;
+  String descripcionP;
+  double precio;
+  DateTime fechaVencimiento;
+
+  Producto(this.codigoP,this.descripcionP,this.nombre, this.precio, this.fechaVencimiento);
+}
+class Venta {
+  int codigoV;
+  String descripcionV;
+  List<Producto> productos = [];
+Venta(this.codigoV,this.descripcionV);
+  void agregarProducto(Producto producto) {
+    productos.add(producto);
+  }
+}
+
+
