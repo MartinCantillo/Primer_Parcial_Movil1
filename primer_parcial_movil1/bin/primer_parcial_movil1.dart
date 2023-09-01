5378e4a8343f5e4a0ec36b87814d3c2b34cae2c9
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-09-01 11:43:47 -0500

    viendo para ver si corre

--- primer_parcial_movil1/bin/primer_parcial_movil1.dart
+++ primer_parcial_movil1/bin/primer_parcial_movil1.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+
 class Producto {
   int codigoP;
   String descripcionP;
@@ -7,7 +8,6 @@ class Producto {
 
   Producto(this.codigoP, this.descripcionP, this.precio, this.diasVencimiento);
 }
-
 
 class Venta {
   int codigoV;
@@ -32,8 +32,30 @@ class Venta {
     return total;
   }
 }
-void main(List<String> arguments) {
 
+void main(List<String> arguments) {
+  Venta venta1 = Venta(1, "Ventas realizadas por Martin :V");
+  print("Ingrese los días de vencimiento para cada producto:");
+
+  Producto portatilLenovo = Producto(1, "Lenovo", 100.0, DiasVencimiento());
+  Producto portatilHP = Producto(2, "HP", 90.0, DiasVencimiento());
+  Producto raton = Producto(3, "ratón", 20.0, DiasVencimiento());
+  Producto pcEscritorio =
+      Producto(4, "PC escritorio", 600.0, DiasVencimiento());
+
+  venta1.agregarProducto(portatilLenovo);
+  venta1.agregarProducto(portatilHP);
+  venta1.agregarProducto(raton);
+  venta1.agregarProducto(pcEscritorio);
+
+  double total = venta1.calcularTotal();
+
+  print("Ventas realizadas por Martin:");
+  for (var producto in venta1.productos) {
+    print("Producto: ${producto.descripcionP}");
+    print("Precio: ${producto.precio}");
+  }
+  print("Total de la venta = $total");
 }
 
 int DiasVencimiento() {
@@ -41,5 +63,3 @@ int DiasVencimiento() {
   String input = stdin.readLineSync()!;
   return int.parse(input);
 }
-
-
